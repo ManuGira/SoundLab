16e81b8b421bc5fbbf6e27801ae160b800487a68
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2022-03-07 23:22:39 +0100

    MOD(git): handle octaves and repetitions parameters

--- RissetBeat/risset_generator.py
+++ RissetBeat/risset_generator.py
@@ -40,16 +40,20 @@ def time_map_eponentially_accelerated(N, final_speed):
     return ns
 
 
-def generate_risset_beat(sound_array, repetitions=4):
+def generate_risset_beat(sound_array, repetitions=4, octaves=3):
     """
     time speed goes from x1 to x2
     """
-    samples1 = sound_array.copy()
-    samples1 = np.tile(samples1, (repetitions, 1))
-    samples2 = double_time_speed(samples1)
+
+    samples = []
+    samples.append(sound_array.copy())
+    samples[0] = np.tile(samples[0], (repetitions, 1))
+    for i in range(1, octaves):
+        new_octave = double_time_speed(samples[i-1])
+        samples.append(new_octave)
 
     # define time remapping
-    N = len(samples1)
+    N = len(samples[0])
     ns = time_map_eponentially_accelerated(N, 2)
     M = len(ns)
 
@@ -57,30 +61,34 @@ def generate_risset_beat(sound_array, repetitions=4):
     ns = np.round(ns).astype(int)
 
     # resample (remap time)
-    samples1 = samples1[ns]
-    samples2 = samples2[ns]
+    for i in range(octaves):
+        samples[i] = samples[i][ns]
 
     # fade in/out first and last layers
     fade_in = np.linspace(0, 1, M)
     fade_out = np.linspace(1, 0, M)
     fade_in = fade_in.reshape(-1, 1)
     fade_out = fade_out.reshape(-1, 1)
-    samples1 = samples1*fade_in
-    samples2 = samples2*fade_out
+    samples[0] = samples[0]*fade_in
+    samples[-1] = samples[-1]*fade_out
 
     # mix layers and normalize to [-1, 1]
-    samples = samples1 + samples2
+    samples = sum(samples)
     max_amp = np.max(np.abs(samples.flatten()))
     samples /= max_amp
     return samples
 
 
 def main():
-    fs, data_int16 = wavfile.read("assets/Risset2.wav")
+    filename = "indus lofi.wav"
+    fs, data_int16 = wavfile.read(f"assets/{filename}")
     data = data_int16.copy() / (2 ** 15 - 1)
-    samples = generate_risset_beat(data)
+    # gui(data)
+    data = np.repeat(data, 2, axis=0)
+    samples = generate_risset_beat(data, repetitions=2, octaves=3)
 
-    wavfile.write("outputs/risset.wav", fs, samples)
+    samples = np.tile(samples, (4, 1))
+    wavfile.write(f"outputs/{filename}", fs, samples)
 
 if __name__ == '__main__':
     main()
